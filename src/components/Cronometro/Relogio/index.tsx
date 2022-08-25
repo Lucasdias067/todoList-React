6c97be4027f810f9b9c8a06a0ba1b21958c82893
Lucas Dias <lucasdias067@gmail.com>
2022-08-25 19:38:46 -0300

    lançando projeto finalizado

---
+++ src/components/Cronometro/Relogio/index.tsx
@@ -0,0 +1,23 @@
+import style from './Relogio.module.scss';
+
+interface Props {
+  tempo: number | undefined;
+}
+
+export default function Relogio({tempo = 0}: Props) {
+  const minutos = Math.floor(tempo / 60);
+  const segundos = tempo % 60;
+  const [minutosDezena, minutosUnidade] = String(minutos).padStart(2, '0');
+  const [segundosDezena, segundosUnidade] = String(segundos).padStart(2, '0');
+
+  return (
+    <>
+      <span className={style.relogioNumero}>{minutosDezena}</span>
+      <span className={style.relogioNumero}>{minutosUnidade}</span>
+      <span className={style.relogioDivisao}>:</span>
+      <span className={style.relogioNumero}>{segundosDezena}</span>
+      <span className={style.relogioNumero}>{segundosUnidade}</span>
+    </>
+  )
+}
+
---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,43 @@
+import Botao from "../Botao";
+import Relogio from "./Relogio";
+import style from './Cronometro.module.scss';
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
+
+interface Props {
+  selecionado: ITarefa | undefined;
+  finalizarTarefa: () => void;
+}
+
+export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado])
+
+  function regressiva(cont: number = 0) {
+    setTimeout(() => {
+      if (cont > 0) { 
+        setTempo(cont - 1);
+        return regressiva (cont - 1);
+      }
+      finalizarTarefa();
+    }, 1000)
+  }
+
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+      <div className={style.relogioWrapper}>
+        <Relogio tempo={tempo} />
+      </div>
+      <Botao onClick={() => regressiva(tempo)}>
+        Começar!
+      </Botao>
+    </div>
+  )
+}
